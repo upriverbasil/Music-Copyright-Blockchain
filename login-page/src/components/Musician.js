c55b199940609ccd246885c81c4b0c625470fb7e
Ritikgarg1773 <ritik18305@iiitd.ac.in>
2021-11-10 17:29:56 +0530

    Update Musician.js

--- login-page/src/components/Musician.js
+++ login-page/src/components/Musician.js
@@ -1,3 +1,81 @@
-export default function Musician(){
-	return(<div></div>)
-}
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+const defaultValues = {
+  title:"",
+  album:"",
+  publishingyear:"",
+  artist:"",
+  fileupload:null
+};
+const User = () => {
+  const [formValues, setFormValues] = useState(defaultValues);
+  const handleInputChange = (e) => {
+
+    const { name, value } = e.target;
+    
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid container alignItems="center" justify="center" direction="column" spacing={2}>
+        <Grid item xs={1}>
+          <TextField
+          	required={true}
+            id="title-input"
+            name="title"
+            label="Title"
+            type="text"
+            value={formValues.title}
+            onChange={handleInputChange}
+          />
+        </Grid>
+        <Grid item xs={1}>
+          <TextField
+          	required={true}
+            id="album-input"
+            name="album"
+            label="Album Name"
+            type="text"
+            value={formValues.album}
+            onChange={handleInputChange}
+          />
+        </Grid>
+         <Grid item xs={1}>
+          <TextField
+          	required={true}
+            id="publishing-input"
+            name="publishingyear"
+            label="Publishing Year"
+            type="integer"
+            value={formValues.publishingyear}
+            onChange={handleInputChange}
+          />
+        </Grid>
+         <Grid item xs={1}>
+          <TextField
+          	required={true}
+            id="artist-input"
+            name="artist"
+            label="Artist/Song Writer/Composition Owner"
+            type="text"
+            value={formValues.artist}
+            onChange={handleInputChange}
+          />
+        </Grid>
+        <Button variant="contained" color="primary" type="submit" >
+          Submit
+        </Button>
+      </Grid>
+    </form>
+  );
+};
+export default User;
