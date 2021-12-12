416373e96d8a6f56ac7a22b91faf97dac8f6ef58
Gavish Gupta <gavishgupta@Gavishs-MacBook-Air.local>
2021-12-12 20:38:34 +0530

    transaction working

--- src/contracts/Musician.sol
+++ src/contracts/Musician.sol
@@ -5,8 +5,8 @@ contract Musicians{
 			string title;
 		    string album_name;
 			uint year;
-			address[] recepients;
-			uint[] payments;
+			string recepients;
+			uint payments;
 	}
 	int public num_musicians;
 	mapping(string=>mapping(string=>Album)) public musician;
@@ -14,8 +14,12 @@ contract Musicians{
 	mapping(string=>mapping(string=>bool)) public album_exists;
 	string[] public all_public_keys;
 	string[] public all_ipfs_hash;
+	// mapping(string=>mapping(string=>address[])) public recepients;
+	// mapping(string=>mapping(string=>uint[])) public payments;
+	// mapping(string=>mapping(string=>uint)) public recepients_length;
+	// mapping(string=>mapping(string=>uint)) public payments_length;
 	function addAlbum(string memory _publickey, string memory _ipfs, string memory _title, string memory _album_name,
-	                   uint _year, address[] memory _recepients, uint[] memory _payments) public {
+	                   uint _year, string memory _recepients, uint _payments) public {
 	    
 	    if(!musician_exists[_publickey]){
 	        //musician doesn't exists
@@ -25,14 +29,11 @@ contract Musicians{
 	    }
 	    all_ipfs_hash.push(_ipfs);
 	    musician[_publickey][_ipfs] = Album(_title,_album_name,_year,_recepients,_payments);
+		
 	    album_exists[_publickey][_ipfs] = true;
 	    
 	}
-	function getAlbum(string memory _publickey, string memory _ipfs) public{
-		all_ipfs_hash.push(_ipfs);
-	}
-	
-    
+	    
 }    
     
     
